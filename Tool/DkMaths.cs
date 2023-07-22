13ef97c27822925b6802a66d455be24d2a84a3d3
DarkCompet <darkcompet@gmail.com>
2023-07-22 17:01:22 +0700

    Change int to long for pow

--- Tool/DkMaths.cs
+++ Tool/DkMaths.cs
@@ -1,21 +1,18 @@
 
 public class DkMaths {
+	/// Fast pow with O(logN) time.
 	/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
 	/// Method 2: x^9 = x^4 * x^4 * x
-	public static int Pow(int x, uint n) {
-		if (n == 0) {
-			return 1;
-		}
-
-		var result = 1;
+	public static long Pow(long x, uint n) {
+		var result = 1L;
 		while (n > 0) {
 			// Mul if meet bit 1
 			if ((n & 1) == 1) {
-				result = result * x;
+				result = (result * x); // Mod here
 			}
 			// Down n and Up x
 			n >>= 1;
-			x = x * x;
+			x = (x * x); // Mod here
 		}
 
 		return result;
