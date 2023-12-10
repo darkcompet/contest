9d376f73e8a77caddff195de5fffbe70f4a4a5a6
DarkCompet <darkcompet@gmail.com>
2023-12-10 19:20:50 +0700

    Add pow with modulo

--- Tool/DkMaths.cs
+++ Tool/DkMaths.cs
@@ -1,6 +1,7 @@
 
 public class DkMaths {
-	/// Fast pow with O(logN) time.
+	/// Fast pow.
+	/// Time: O(logN)
 	/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
 	/// Method 2: x^9 = x^4 * x^4 * x
 	public static long Pow(long x, uint n) {
@@ -8,11 +9,30 @@ public class DkMaths {
 		while (n > 0) {
 			// Mul if meet bit 1
 			if ((n & 1) == 1) {
-				result = (result * x); // Mod here
+				result *= x;
 			}
 			// Down n and Up x
 			n >>= 1;
-			x = (x * x); // Mod here
+			x *= x;
+		}
+
+		return result;
+	}
+
+	/// Fast pow with modulo.
+	/// Time: O(logN)
+	/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
+	/// Method 2: x^9 = x^4 * x^4 * x
+	public static long Pow(long x, uint n, int mod) {
+		var result = 1L;
+		while (n > 0) {
+			// Mul if meet bit 1
+			if ((n & 1) == 1) {
+				result = result * x % mod;
+			}
+			// Down n and Up x
+			n >>= 1;
+			x = x * x % mod;
 		}
 
 		return result;
